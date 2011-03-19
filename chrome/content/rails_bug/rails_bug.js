c8d56729171c886b1af9eeca925b558e627cb2e7
Leonid Shevtsov <leonid@shevtsov.me>
2011-03-19 02:04:34 +0200

    observer bound to context working

--- chrome/content/rails_bug/rails_bug.js
+++ chrome/content/rails_bug/rails_bug.js
@@ -2,38 +2,60 @@ FBL.ns(function() { with (FBL) {
 
   var panelName = "RailsBug";
 
-  Firebug.RailsBugModule = extend(Firebug.Module, {
+  function RailsBugObserver(context) {
+    this.context = context;
+  }
 
-    initialize: function() {
-      Firebug.Module.initialize.apply(this, arguments);
-
+  RailsBugObserver.prototype = {
+    observe: function(subject, topic, data) {
+      if (Firebug.RailsBugModule.activeContext == this.context) {
+        if (topic == 'http-on-modify-request') {
+          var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
+          httpChannel.setRequestHeader('X-RailsBug-Enabled', 'true', false);
+        } else if (topic == 'http-on-examine-response') {
+          try { 
+            var rails_bug_data = JSON.parse(subject.getResponseHeader('X-RailsBug'));
+            Firebug.RailsBugModule.handle(context, rails_bug_data);
+          } catch(NS_ERROR_NOT_AVAILABLE) {
+            // no RailsBug header present
+          }
+        }
+      }
+    },
+      
+    bind: function() {
       httpRequestObserver.addObserver(this, 'firebug-http-event', false);
     },
 
-    shutdown: function() {
-      Firebug.Module.shutdown.apply(this, arguments);
-
+    unbind: function() {
       httpRequestObserver.removeObserver(this, 'firebug-http-event');
+    }
+  };
+
+  Firebug.RailsBugModule = extend(Firebug.Module, {
+    activeBrowser: null,
+    activeContext: null,
+
+    showContext: function(browser, context) {
+      this.activeBrowser = browser;
+      this.activeContext = context;
+    },
+    
+    initContext: function(context, persisted) {
+      context.railsBugObserver = new RailsBugObserver(context);
+      context.railsBugObserver.bind();
+    },
+  
+    destroyContext: function(context, persisted) {
+      context.railsBugObserver.unbind();
+      delete context.railsBugObserver;
     },
 
     onMyButton: function(context) {
     },
 
-    observe: function(subject, topic, data) {
-      if (topic == 'http-on-modify-request') {
-        var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
-        httpChannel.setRequestHeader('X-RailsBug-Enabled', 'true', false);
-      } else if (topic == 'http-on-examine-response') {
-        try { 
-          var data = JSON.parse(subject.getResponseHeader('X-RailsBug'));
-          this.handleRailsBug(data);
-        } catch(NS_ERROR_NOT_AVAILABLE) {
-          // no RailsBug header present
-        }
-      }
-    },
-
-    handleRailsBug: function(data) {
+    handle: function(context, data) {
+      FBTrace.sysout('data', data);
     }
   });
 
